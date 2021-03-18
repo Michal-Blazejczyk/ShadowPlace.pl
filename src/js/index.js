db2cf60236e7959f2d08ea3264922753ee5cde46
Michal <budzik555@interia.pl>
2021-03-18 18:46:54 +0000

    new hamburger menu style

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import '../scss/main.scss';
+/* import './burger.js'; */
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
--- src/js/tooltip.js
+++ src/js/tooltip.js
@@ -1,15 +1,16 @@
-function createTooltips()
-{
-    var elementsWithTooltip = document.getElementsByClassName("tooltip");
+
+        function createTooltips()
+      {
+        var elementsWithTooltip = document.getElementsByClassName("tooltip");
     
-    var tooltipContainer = document.createElement("div");
-    tooltipContainer.id = "tooltipContainer";
-    document.body.appendChild(tooltipContainer);
+        var tooltipContainer = document.createElement("div");
+        tooltipContainer.id = "tooltipContainer";
+        document.body.appendChild(tooltipContainer);
     
-    var tmpTitles = [];
+        var tmpTitles = [];
     
-    for (var i = 0; i < elementsWithTooltip.length; i++)
-    {
+        for (var i = 0; i < elementsWithTooltip.length; i++)
+          {
         tmpTitles[i] = elementsWithTooltip[i].title;
         
         elementsWithTooltip[i].tmp_id = i;
@@ -32,5 +33,35 @@ function createTooltips()
           
            tooltipContainer.style.display = "none";
         });        
+        }
+          }
+      window.onload = function()
+      {
+      createTooltips();
+      var mainImage = document.getElementById("mainImage");
+      var image = new Image();
+    
+      mainImage.appendChild(image);
+    
+      var thumbnails = document.getElementsByClassName("thumbnail");
+    
+      var currentThumbnail = thumbnails[0];
+    
+      image.src = currentThumbnail.getAttribute("src");
+      currentThumbnail.className += " current";
+    
+      for (var i = 0; i < thumbnails.length; i++)
+      {
+        thumbnails[i].addEventListener("mouseover",function()
+        {
+          
+            currentThumbnail.className = currentThumbnail.className.replace("current", "");
+            currentThumbnail = this;
+            currentThumbnail.className += " current";
+            
+            image.src = this.getAttribute("src");
+        });
+      }
+    
     }
-}
+   
